d0ca2d5127da2f79917230badfe4dc795c1a4ce7
Lou Foster <lou@performantsoftware.com>
2012-08-22 11:50:24 -0400

    specify memory constraints on start

--- juxta-ws/scripts/start.sh
+++ juxta-ws/scripts/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "Launching Juxta WS"
-java -jar -server -XX:+OptimizeStringConcat juxta-ws.jar &
+java -jar -server -Xmn100M -Xms1G -Xmx1G -XX:+OptimizeStringConcat juxta-ws.jar &
