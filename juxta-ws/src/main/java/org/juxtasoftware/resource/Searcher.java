a4531c639a73f1a810f00f0f8e004894d6db0816
Lou Foster <lou@performantsoftware.com>
2012-10-11 14:06:54 -0400

    work towards getting positions of search hits

--- juxta-ws/src/main/java/org/juxtasoftware/resource/Searcher.java
+++ juxta-ws/src/main/java/org/juxtasoftware/resource/Searcher.java
@@ -2,6 +2,11 @@ package org.juxtasoftware.resource;
 
 import java.io.IOException;
 
+import org.apache.lucene.document.Document;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.index.TermFreqVector;
+import org.apache.lucene.index.TermPositionVector;
+import org.apache.lucene.index.TermVectorOffsetInfo;
 import org.apache.lucene.queryParser.ParseException;
 import org.apache.lucene.queryParser.QueryParser;
 import org.apache.lucene.search.IndexSearcher;
@@ -29,6 +34,7 @@ public class Searcher extends BaseResource {
     
     @Autowired private IndexSearcher searcher;
     @Autowired private QueryParser queryParser;
+    @Autowired private IndexReader indexReader;
 
 
     @Override
@@ -46,6 +52,33 @@ public class Searcher extends BaseResource {
         try {
             Query query = this.queryParser.parse(this.searchTerm);
             ScoreDoc[] hits = this.searcher.search(query, 100).scoreDocs;
+            
+//            for(int i=0;i<hits.length;++i) {  
+//
+//                int docId = hits[i].doc;  
+//                TermFreqVector tfvector = this.indexReader.getTermFreqVector(docId, "content");  
+//                TermPositionVector tpvector = (TermPositionVector)tfvector;  
+//                // this part works only if there is one term in the query string,  
+//                // otherwise you will have to iterate this section over the query terms.  
+//                int termidx = tfvector.indexOf(querystr);  
+//                int[] termposx = tpvector.getTermPositions(termidx);  
+//                TermVectorOffsetInfo[] tvoffsetinfo = tpvector.getOffsets(termidx);  
+//
+//                for (int j=0;j<termposx.length;j++) {  
+//                    System.out.println("termpos : "+termposx[j]);  
+//                }  
+//                for (int j=0;j<tvoffsetinfo.length;j++) {  
+//                    int offsetStart = tvoffsetinfo[j].getStartOffset();  
+//                    int offsetEnd = tvoffsetinfo[j].getEndOffset();  
+//                    System.out.println("offsets : "+offsetStart+" "+offsetEnd);  
+//                }  
+//
+//                // print some info about where the hit was found...  
+//                Document d = searcher.doc(docId);  
+//                System.out.println((i + 1) + ". " + d.get("path"));  
+//            }  
+            
+            
             return toTextRepresentation("Got "+hits.length+" matches");
         } catch (ParseException e) {
             setStatus(Status.CLIENT_ERROR_BAD_REQUEST);
@@ -55,6 +88,10 @@ public class Searcher extends BaseResource {
             setStatus(Status.SERVER_ERROR_INTERNAL);
             LOG.error("Search failed", e);
             return toTextRepresentation("Search Failed");
+        } finally {
+            try {
+                this.searcher.close();
+            } catch (IOException e) {}
         }
     }
 }
