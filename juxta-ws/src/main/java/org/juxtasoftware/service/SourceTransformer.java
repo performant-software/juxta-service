089b769d7f84a5c40e851b51b81f6b59801fded6
Lou Foster <lou@performantsoftware.com>
2012-11-14 09:35:15 -0500

    transform wiki sources to witnesses

--- juxta-ws/src/main/java/org/juxtasoftware/service/SourceTransformer.java
+++ juxta-ws/src/main/java/org/juxtasoftware/service/SourceTransformer.java
@@ -34,6 +34,7 @@ import org.juxtasoftware.model.RevisionInfo;
 import org.juxtasoftware.model.Source;
 import org.juxtasoftware.model.Witness;
 import org.juxtasoftware.util.HtmlUtils;
+import org.juxtasoftware.util.WikiTextUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Scope;
@@ -94,7 +95,9 @@ public class SourceTransformer {
             parsedContent = doTransform(srcDoc, xslt);
         } else if  ( srcDoc.getType().equals(Source.Type.HTML) ) {
             parsedContent = doHtmlTransform(srcDoc);
-        } else {
+        } else if  ( srcDoc.getType().equals(Source.Type.WIKI) ) {
+            parsedContent = doWikiTransform(srcDoc);
+        }else {
             NullTransformReader rdr = new NullTransformReader();
             this.textRepository.read(srcDoc.getText(), rdr);
             parsedContent = rdr.getContent();
@@ -125,13 +128,15 @@ public class SourceTransformer {
      */
     public Long transform(final Source srcDoc, final JuxtaXslt xslt, final String finalName) throws SAXException, IOException, TransformerException {        
         String witnessName = finalName;
-
+        
         // transform into a new text_content object        
         Text parsedContent = null;
         if (srcDoc.getType().equals(Source.Type.XML)) {     
             parsedContent = doTransform(srcDoc, xslt);
         } else if  ( srcDoc.getType().equals(Source.Type.HTML) ) {
             parsedContent = doHtmlTransform(srcDoc);
+        } else if  ( srcDoc.getType().equals(Source.Type.WIKI) ) {
+            parsedContent = doWikiTransform(srcDoc);
         } else {
             NullTransformReader rdr = new NullTransformReader();
             this.textRepository.read(srcDoc.getText(), rdr);
@@ -156,6 +161,16 @@ public class SourceTransformer {
         }
         
         return id;
+    }
+    
+    private Text doWikiTransform(Source srcDoc) throws IOException {
+        File htmlOut = WikiTextUtils.toTxt( new ReaderInputStream(this.sourceDao.getContentReader(srcDoc), "UTF-8") );
+        FileInputStream fis = new FileInputStream(htmlOut);
+        InputStreamReader isr = new InputStreamReader(fis, "UTF-8");
+        Text parsedContent = this.textRepository.create( isr );
+        isr.close();
+        htmlOut.delete();
+        return parsedContent;
     }
 
     private Text doHtmlTransform(Source srcDoc) throws IOException {
