ff5ddd4cd096f4db4f84d7f8c9f55efc67f2c363
Lou Foster <lou@performantsoftware.com>
2013-01-03 09:13:25 -0500

    increased starting min memory

--- juxta-ws/scripts/start.sh
+++ juxta-ws/scripts/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "Launching Juxta WS"
-java -jar -server -Xmn100M -Xms1G -Xmx1G -XX:+OptimizeStringConcat juxta-ws.jar &
+java -jar -server -Xmn512M -Xms1G -Xmx1G -XX:+OptimizeStringConcat juxta-ws.jar &
